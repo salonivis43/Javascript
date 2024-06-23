92ec275a75666253434230a2399178c936d4c240
salonivis43 <159527103+salonivis43@users.noreply.github.com>
2024-06-23 07:43:35 +0000

    memory

---
+++ 01_basics/05_memory.js
@@ -0,0 +1,19 @@
+//stack(primitive) , heap(non-primitive)
+
+let myName ="Saloni"
+anotherNme ="moni"
+
+console.log(myName);
+console.log(anotherNme);
+
+let userOne={
+    name:"Saloni",
+    upi:"user@ybl"
+}
+
+let userTwo=userOne
+
+userTwo.email="addf@gmail"
+
+console.log(userOne,email);
+console.log(userTwo.email);
