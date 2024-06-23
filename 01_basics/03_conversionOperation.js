573a97689bec1ee4b62b5c30b5404ce66a09650d
salonivis43 <159527103+salonivis43@users.noreply.github.com>
2024-06-23 07:35:29 +0000

    complate datatype

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,69 @@
+// // let score ="33abc"
+
+// let score ="null"
+
+
+// console.log(typeof score);
+
+// let valueinNumber =Number(score)
+
+// console.log(typeof valueinNumber);
+// console.log(valueinNumber);
+
+// // "33" =>33
+// // "33abc"=> NaN
+// // true =>1 ;false >0
+
+// let isLoggedin = 1
+
+// let booleanIsLoggedIn = Boolean(isLoggedin)
+// console.log(booleanIsLoggedIn)
+
+// // 1=> true ; 0=> false
+
+// //" =>false
+// // "asd " =>true
+
+
+// let someNumber=32
+// let strimgNumber = String(someNumber)
+// console.log(strimgNumber)
+// console.log(typeof strimgNumber)
+
+//****************************Operations************************** */
+
+let value =3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2/2)
+// console.log(2**2)
+// console.log(2%2)
+
+let str1 ="hello"
+let str2 =" Saloni"
+let str3 =str1+str2
+console.log(str3)
+
+console.log("1"+2)
+console.log(1+"2")
+console.log("1"+"2")
+console.log("1"+2+22)
+console.log(1+2 + "2 ")
+
+console.log((3+4)*5 % 3);
+
+console.log(+true);
+console.log(+"");
+
+let num1,num2,num3;
+
+num1=num2=num3 = 2+2
+
+let gameCounter =100
+console.log(gameCounter)
+gameCounter++
+console.log(gameCounter)
---
+++ 01_basics/datatype_summy.js
@@ -0,0 +1,19 @@
+// primitive (value type) 
+// 7 type : String , number , Boolean , null, undefined ,symbol ,BigInt
+
+
+
+// Non primitive (reference type)\
+// Array , object , Funtions
+
+const hero =["saloni ","soni","sdfg"];
+
+let myobj ={
+    name:"Salomni",
+    age:20
+}
+
+const myFunction =function(){
+console.log("hello");
+}
+
